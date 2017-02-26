0fe0f5e75af8ace6bb7c7c3ace0ca40fbed52b26
kaspar.mart@gmail.com <kaspar.mart@gmail.com>
2017-02-26 03:09:36 +0100

    test gitkraken
    ddd

--- src/main/java/cz/mka/impl/dao/DrinkerDao.java
+++ src/main/java/cz/mka/impl/dao/DrinkerDao.java
@@ -8,14 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface DrinkerDao extends JpaRepository<DrinkerDTO, Long> {
 
-    // save new drinker - by JpaRepository
-
-    // retrieve all drinkers - by JpaRepository
-
-    // find one drinker - by JpaRepository
-
-    // update drinker - by JpaRepository
-
-    // delete drinker - by JpaRepository
-
 }
